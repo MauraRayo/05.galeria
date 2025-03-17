899d24575363e9ed296774b52b797b0e690ff83e
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2025-03-17 17:42:33 +0100

    Movimiento mouse

--- js/mouse.js
+++ js/mouse.js
@@ -2,7 +2,12 @@
 
 let pm ={
 
-    zona:  document.querySelector("#efectoMouse")
+    zona:  document.querySelector("#efectoMouse"),
+    figuras: document.querySelectorAll("#efectoMouse figure "),
+    mouseX: 0,
+    mouseY: 0,
+    horizontal: true,
+    vertical : false,
 
 }
 
@@ -12,12 +17,34 @@ let pm ={
 
 let mm = {
     inicioMouse: function() {
-        pm.zona.addEventListener("mousemove", mm.movimietoMouse)
-        
+        pm.zona.addEventListener("mousemove", mm.movimietoMouse);
+
+        for (let i = 0; i < pm.figuras.length; i++) {
+                   
+            pm.figuras[i].innerHTML = '<img src="img/mouse/plano0' +i+ '.png">';
+            pm.figuras[i].style.zIndex= -i;
+            
+        }
+        // capturar la altura
+        setTimeout(function() {
+          pm.zona.style.height =  pm.figuras[0].childNodes[0].height +"px";
+            
+        },100)
     },
     movimietoMouse: function(mouse) {
-        console.log(mouse.offsetX, mouse.offsetY)
-        
+        pm.mouseX = -mouse.offsetX;
+        pm.mouseY = mouse.offsetY;
+
+        for (let i = 0; i < pm.figuras.length; i++) {
+
+            if (pm.horizontal) {
+                pm.figuras[i].style.left= pm.mouseX/(i*100+50) + "%";                
+            }
+            if(vertical){
+
+                pm.figuras[i].style.Top=  pm.mouseY/(i*100+50) + "%";
+            }
+        } 
     }
 
 }
