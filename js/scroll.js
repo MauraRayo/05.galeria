1d3bb1296f589e7c3838b5ccc1285d53d3fb60a4
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2025-04-04 11:42:10 +0200

    Botones con el scrolll
    vincular los botones con el Scroll

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,11 @@ let ps = {
     posicionScroll: 0,
     articulos: document.querySelectorAll("#scroll article"),
     cajaScroll: document.querySelector("#scroll"),
-    headerScroll: document.querySelector("header")
+    headerScroll: document.querySelector("header"),
+    botonera: document.querySelectorAll("nav ul li a"),
+    ruta: null,
+    intervalo: null, 
+    destinoScroll: 0
  
   
 }
@@ -21,9 +25,13 @@ let ps = {
 
 let ms = {
     incioScroll: function () {
-     document.addEventListener("scroll", ms.efectoParallax);
+     document.addEventListener("scroll", ms.efectoParallax)
+     for(let i =0; i < ps.botonera.length; i++){
+         ps.botonera[i].addEventListener("click", ms.desplazamiento)
+        }
         
     },
+
 
     efectoParallax: function(){
        ps.posicionScroll= window.pageYOffset;
@@ -49,6 +57,29 @@ let ms = {
             }
 
         } 
+    },
+    desplazamiento: function(ruta){
+        ruta.preventDefault();
+
+      ps.ruta = ruta.target.getAttribute("href");
+      
+      ps.destinoScroll = document.querySelector(ps.ruta).offsetTop
+
+      ps.intervalo = setInterval(function(){
+        if(ps.posicionScroll < ps.destinoScroll){
+            ps.posicionScroll += 100;
+
+            if(ps.posicionScroll >= ps.destinoScroll){
+                ps.posicionScroll = ps.destinoScroll;
+                clearInterval(ps.intervalo)
+            }
+
+        }
+         window.scrollTo(0,  ps.posicionScroll);
+
+      }, 50)
+
+
     }
 
 }
